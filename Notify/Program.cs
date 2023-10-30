ce4f278c2346a02c2b43e82b26fc7f1fc69a477d
Stanislav <slava.telyatnik@mail.ru>
2023-10-30 14:05:39 +0100

    Добавьте файлы проекта.

---
+++ Notify/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            try
+            {
+                int[] array = new int[3];
+                array[2] = 10;
+                int a = 10;
+                int b = 2;
+                int result = a / b;
+                string str = "jhwusc";
+                int l = str.Length + array.Length;
+                throw new MyException("Мое исключение");
+            }
+            catch (MyException ex)
+            { 
+                Console.WriteLine("Поймали свое исключение:");
+                Console.WriteLine(ex.Message);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Поймали исключение IndexOutOfRangeException:");
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Поймали исключение DivideByZeroException:");
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Поймали исключение NullReferenceException:");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Поймали другое исключение:");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        
+
+        public class MyException : Exception
+        {
+            public MyException()
+            { }
+
+            public MyException(string message)
+                : base(message)
+            { }
+        }
+    }
+}
